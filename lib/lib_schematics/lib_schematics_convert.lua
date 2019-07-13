8a8da689ce0daa2f984563362bef39259c39123a
Shad MOrdre <shadmordre@gmail.com>
2019-07-13 11:04:32 -0700

    Add files via upload

---
+++ lib/lib_schematics/lib_schematics_convert.lua
@@ -0,0 +1,17 @@
+
+--Schematics Conversion Library
+
+game.lib.schematics.convert = {}
+game.lib.schematics.convert.name = "lib_schematics_convert"
+game.lib.schematics.convert.path = game.lib.schematics.path .. "/lib_schematics_convert"
+game.lib.schematics.convert.ver_maj = 0
+game.lib.schematics.convert.ver_min = 0
+game.lib.schematics.convert.ver_rev = 1
+
+game.lib.schematics.convert.mts2lua = dofile(game.lib.schematics.convert.path .. "/lib_schematics_convert_mts2lua.lua")
+--game.lib.schematics.convert.mts2we
+--game.lib.schematics.convert.lua2mts
+--game.lib.schematics.convert.lua2we
+--game.lib.schematics.convert.we2lua
+--game.lib.schematics.convert.we2mts
+
