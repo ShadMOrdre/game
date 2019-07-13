f424b3dd940f7f5627ab4537589955540d3109cc
Shad MOrdre <shadmordre@gmail.com>
2019-07-13 10:58:11 -0700

    Add files via upload

--- lib/lib_io/lib_io_get_file_list_by_type.lua
+++ lib/lib_io/lib_io_get_file_list_by_type.lua
@@ -1,19 +1,26 @@
 
 --function returns list of files in current dir
 
-local function get_file_list(file_list_path)
+local function get_file_list(file_type, file_list_path)
 
 	local file_list = minetest.get_dir_list( file_list_path, false );
 
 	local idx_file_list = 0
 	if file_list then
 
-		for _,filename in ipairs( file_list ) do	
-	
+		for _,filename in ipairs( file_list ) do
+		
 			idx_file_list = idx_file_list + 1
 
 			local schemname = filename;
+			local i = string.find(filename, file_type,  -4 );
+			if( i ) then
+				schemname = string.sub( filename, 1, i-1 );
+			else
 
+			end
+
+			-- only add known file types
 			if( not( schemname )) then
 				return;
 			end
