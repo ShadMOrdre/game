b97e265046e3d4fdae5727a65a239e91f9e90d2d
Shad MOrdre <shadmordre@gmail.com>
2019-07-13 10:49:54 -0700

    Update game_lib.lua

--- game_lib.lua
+++ game_lib.lua
@@ -1,14 +1,35 @@
 
 --library object.
 
-game.library.path = game.path_mod .. "/lib"
-game.path_lib = game.library.path
+game.lib.path = game.path_mod .. "/lib"
+game.path_lib = game.lib.path
 
-game.library.ver_maj = 0
-game.library.ver_min = 0
-game.library.ver_rev = 1
+game.lib.ver_maj = 0
+game.lib.ver_min = 0
+game.lib.ver_rev = 1
 
 
 
-dofile(game.library.path .. "/lib_csv.lua")
+dofile(game.lib.path .. "/lib_io.lua")
+
+dofile(game.lib.path .. "/lib_table.lua")
+
+dofile(game.lib.path .. "/lib_csv.lua")
+
+dofile(game.lib.path .. "/lib_config.lua")
+
+
+dofile(game.lib.path .. "/lib_node.lua")
+
+dofile(game.lib.path .. "/lib_schematics.lua")
+
+dofile(game.lib.path .. "/lib_mapgen.lua")
+
+dofile(game.lib.path .. "/lib_biome.lua")
+
+dofile(game.lib.path .. "/lib_ore.lua")
+
+dofile(game.lib.path .. "/lib_ecosystem.lua")
+
+dofile(game.lib.path .. "/lib_decoration.lua")
 
